a9026901edf3b375a3739439bb4d05b5ce536728
ayato_p <ayato.skycrawlers@gmail.com>
2017-02-09 08:11:21 +0900

    [integrant-blog] migrate -> migrator

--- integrant-blog/project.clj
+++ integrant-blog/project.clj
@@ -1,22 +1,32 @@
 (defproject demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "building blog app with integrant"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; lifecycle
                  [integrant "0.2.0"]
+
+                 ;; server
                  [org.immutant/web "2.1.6"]
                  [ring/ring-core "1.5.1"]
-                 [bidi "2.0.16"]
+
+                 ;; ring middleware
                  [ring/ring-defaults "0.2.1"]
+
+                 ;; routing
+                 [bidi "2.0.16"]
 
                  ;; database
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [org.postgresql/postgresql "9.4.1212"]
                  [ragtime "0.6.3"]
 
+                 ;; sql
+                 [stch-library/sql "0.1.1"]
+
                  ;; config
-                 [rkworks/baum "0.4.0"]]
+                 [rkworks/baum "0.4.0"]
+
+                 ;; webjars
+                 [org.webjars/bootstrap "3.3.7-1"]]
 
   :profiles
   {:dev {:dependencies []
